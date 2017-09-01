4cf090896e17523a9d8ccc953a3f92a33071bb82
ripkort <ripkort@gmail.com>
2017-09-01 11:52:44 +0300

    Second task

--- basics/pythonhw.py
+++ basics/pythonhw.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 
+#===Task #1===
 def hello_string(arg):
     print('Hello, {}'.format(arg))
 
 a = "Vasya"
 hello_string(a)
+
+#===Task #2===
+def sum(_list):
+    count = 0
+    for i in _list: 
+        count += i
+    return count
+
+def multiply(_list):
+    count = 1
+    for i in _list:
+        count *= i
+    return count
+
+l = [1,2,3,4]
+print(sum(l))
+print(multiply(l))
+
